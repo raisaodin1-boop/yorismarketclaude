a9c9fefb5ef2036f2f4fa2522e9b30c57e4de9b7
raisa kouekam <raisaodin1@gmail.com>
2026-03-27 16:37:03 +0100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import Yorix from './Yorix.jsx'
+import App from "./App.jsx"
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Yorix />
+    <App />
   </React.StrictMode>
 )
