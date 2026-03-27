997d965b07c01b460901d6fa9c4e22099deeb187
raisaodin1-boop <raisaodin1@gmail.com>
2026-03-27 17:03:40 +0100

    firebase fixed

---
+++ src/firebase.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyB8OoAoT7u0qbQWkWbISJ6vId9W2TV8PXE",
+  authDomain: "yorix-74030.firebaseapp.com",
+  projectId: "yorix-74030",
+  storageBucket: "yorix-74030.firebasestorage.app",
+  messagingSenderId: "942631804989",
+  appId: "1:942631804989:web:b068e95deed0d5a927ca54"
+};
+
+const app = initializeApp(firebaseConfig);
+
+export default app;
