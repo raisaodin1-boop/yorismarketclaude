f4676c534827cd989b5e8b11aea2f5d1bb78ae3c
raisa kouekam <raisaodin1@gmail.com>
2026-04-12 06:44:12 +0100

    Update styles.js

--- src/utils/styles.js
+++ src/utils/styles.js
@@ -1,7 +1,7 @@
 // ─────────────────────────────────────────────────────────────
 // CSS
 // ─────────────────────────────────────────────────────────────
-const makeCSS = (dark) => `
+export const makeCSS = (dark) => `
 @import url('https://fonts.googleapis.com/css2?family=Syne:wght@400;600;700;800&family=DM+Sans:wght@300;400;500;600&display=swap');
 *{margin:0;padding:0;box-sizing:border-box;}
 :root{
