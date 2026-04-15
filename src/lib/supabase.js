6741ea159a8bf30a54a32a01b2c3f2eccb5f1b68
raisa kouekam <raisaodin1@gmail.com>
2026-04-15 11:41:35 +0100

    Update supabase.js

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -11,5 +11,5 @@ export const MOMO_NUMBER = "676935195";
 export const ORANGE_NUMBER = "696565654";
 export const PAYMENT_WA_NUMBER = "237696565654";
 export const LIVRAISON_FEE = 1500;
-export const CLOUD_NAME = import.meta.env.VITE_CLOUDINARY_CLOUD_NAME || "";
+export const CLOUD_NAME = import.meta.env.VITE_CLOUDINARY_CLOUD_NAME || "dulwb03nf";
 export const UPLOAD_PRESET = import.meta.env.VITE_CLOUDINARY_UPLOAD_PRESET || "yorix_unsigned";
