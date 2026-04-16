1e83845aa7699e7aa6c5257b120b1978695a7969
raisa kouekam <raisaodin1@gmail.com>
2026-04-16 18:21:54 +0100

    Update supabase.js

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,8 +1,8 @@
 import { createClient } from "@supabase/supabase-js";
 
 export const supabase = createClient(
-  "https://msrymchhhxitdevthvdi.supabase.co",
-  "sb_publishable_yJj7JNdn-r19Pjc070IOBg_y2VzGJXA"
+  import.meta.env.VITE_SUPABASE_URL,
+  import.meta.env.VITE_SUPABASE_ANON_KEY
 );
 
 export const COMMISSION_RATE = 0.05;
